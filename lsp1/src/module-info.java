974e34368a9972e2ab7ad48a97f15bf7c6b7c495
Andria Joseph <Andria Joseph@10.188.161.75>
2023-01-26 19:11:40 -0500

    I have completed Homework 1 for large scale programming.

---
+++ lsp1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Andria Joseph
+ *
+ */
+module lsp1 {
+}
---
+++ lsp1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+*  Name: Andria Joseph
+*/
+
+
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
