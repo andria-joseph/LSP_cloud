01926b7656cf5004666f2cb8e6f76b572ba6a390
Andria Joseph <Andria Joseph@DESKTOP-SP3VTCD>
2023-03-16 21:30:05 -0400

    midterm

--- lsp1/src/org/howard/edu/lsp/midterm/problem51/IntegerRange.java
+++ lsp1/src/org/howard/edu/lsp/midterm/problem51/IntegerRange.java
@@ -1,5 +1,37 @@
 package org.howard.edu.lsp.midterm.problem51;
 
-public class IntegerRange {
+import static org.junit.Assert.*;
+import org.junit.Test;
 
+public class IntegerRangeTest {
+    private Range range1 = new IntegerRange(5, 10);
+    private Range range2 = new IntegerRange(8, 15);
+    private Range range3 = new IntegerRange(1, 1);
+
+    @Test
+    public void testContains() {
+        assertTrue(range1.contains(5));
+        assertTrue(range1.contains(10));
+        assertTrue(range1.contains(7));
+        assertFalse(range1.contains(4));
+        assertFalse(range1.contains(11));
+    }
+
+    @Test
+    public void testOverlaps() throws EmptyRangeException {
+        assertTrue(range1.overlaps(range2));
+        assertTrue(range2.overlaps(range1));
+        assertTrue(range1.overlaps(range1));
+        assertFalse(range1.overlaps(new IntegerRange(1, 4)));
+        assertFalse(range1.overlaps(new IntegerRange(11, 15)));
+        assertFalse(range1.overlaps(null));
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(6, range1.size());
+        assertEquals(8, range2.size());
+        assertEquals(1, range3.size());
+    }
 }
+
--- lsp1/src/org/howard/edu/lsp/midterm/problem51/Range.java
+++ lsp1/src/org/howard/edu/lsp/midterm/problem51/Range.java
@@ -1,9 +1,45 @@
 package org.howard.edu.lsp.midterm.problem51;
 
-public class Range {
+import java.util;
+public interface Range { 
+    boolean contains(int value);  
+    boolean overlaps(Range other) throws EmptyRangeException; 
+    int size(); 
+}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class IntegerRange implements Range {
+    private int lower;
+    private int upper;
+
+    IntegerRange(int lower, int upper) {
+        this.lower = lower;
+        this.upper = upper;
+    }
+
+    public int getLower() {
+        return lower;
+    }
+
+    public int getUpper() {
+        return upper;
+    }
+
+    public boolean contains(int value) {
+        return (value >= lower && value <= upper);
+    }
+
+    public boolean overlaps(Range other) throws EmptyRangeException {
+        if (other == null) {
+            throw new EmptyRangeException("Cannot pass null Range object.");
+        }
+        return (lower <= other.getUpper() && other.getLower() <= upper);
+    }
+
+    public int size() {
+        return (upper - lower + 1);
+    }
+}
+
 
 	}
 
